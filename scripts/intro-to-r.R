f1f38255a994e5ac8274dd34537c9229a6915352
bgrimer6 <grime.benjamin@gmail.com>
2024-10-08 10:22:16 -0700

    Initial commit

---
+++ scripts/intro-to-r.R
@@ -0,0 +1,70 @@
+
+important_value <- 3*4
+
+weight_kg <- 25
+
+2.2*weight_kg
+
+# creating a vector
+weight_kg <- c(25, 33, 12)
+
+weight_kg
+
+weight_lb <- weight_kg*2.2
+#create a string (must be in quotation marks)
+science_rocks <- "yes it does!"
+
+class(science_rocks)
+class(important_value)
+
+science_rocks*3
+
+is.numeric(science_rocks) # returns FALSE
+is.character(science_rocks) # returns TRUE
+
+## vectors example
+
+# Character vector
+chr_vector <- c("hello", "goodbye", "see you later")
+
+# Numeric Vector 
+num_vector <- c(5, 1.3, 10)
+
+# Logical vector
+boolean_vector <- c(TRUE, FALSE, FALSE)
+
+class(boolean_vector)
+
+
+
+## using the mean() function - note: you can either add "x=" or just put it first
+weight_lb <- mean(x = weight_lb)
+
+weight_lb <- c(60, 30, 17)
+weight_lb_mean <- mean(weight_lb)
+
+
+## reading data with read.csv()
+bg_chem_dat <- read.csv(file = "data/BGchem2008data.csv") 
+head(bg_chem_dat$Date)
+
+mean(bg_chem_dat$CTD_Temperature)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
